588def3ec83b34c7c6452f79cd669649c76f2812
john.tg <john.tg177@gmail.com>
2023-07-22 20:57:17 +0530

    cart ui update and zustand integration

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -8,26 +8,60 @@ import {
   SheetHeader,
   SheetTitle,
   SheetDescription,
+  SheetPrimitive,
 } from "./ui/Sheet";
 import { Button } from "./ui/Button";
+import { useShoppingCart } from "@/lib/store";
+import React, { useEffect } from "react";
+import { Product } from "@/lib/slices/createProductSlice";
 
 export default function CartButton() {
+  const { cart } = useShoppingCart();
+  const [cartItem, setCartItem] = React.useState<Product[]>([...cart]);
+  const [productItems, setProductItems] = React.useState<Product[]>([...cart]);
+
+  const calculateTotal = () => {
+    return cart.reduce((acc, item) => acc + item.price * item.quantity!, 0);
+  };
+
+  useEffect(() => {
+    setCartItem(cart);
+    // calculateTotal();
+  }, [cart]);
+
+  console.log(cartItem.map.length);
+
   return (
     <Sheet>
       <SheetTrigger asChild>
         <Button
           variant="outline"
-          className="cursor-pointer inline-flex items-center space-x-2 justify-center rounded-md text-sm shadow-sm"
+          className="relative cursor-pointer inline-flex items-center space-x-2 justify-center rounded-md  shadow-sm"
         >
-          <ShoppingCart className="w-4 h-4" />
+          <ShoppingCart className=" w-4 h-4" />
+          {cartItem.map.length > 0 && (
+            <span className="absolute bg-secondary rounded-full w-6 h-6 text-secondary-foreground flex items-center justify-center text-xs -top-2 -right-2">
+              1
+            </span>
+          )}
         </Button>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="lg: w-full">
         <SheetHeader>
-          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+          <SheetTitle>Cart</SheetTitle>
           <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            <div>
+              <div className="col-span-9 mt-4 gap-2 rounded-md border-2 border-dashed border-gray-200 p-6 text-center flex items-center justify-center flex-col h-[200px] md:h-[150px]">
+                <h1 className="text-md font-medium text-primary tracking-tight">
+                  Your cart is empty
+                </h1>
+              </div>
+              <SheetPrimitive.Close className="w-full">
+                <Button variant="default" className="mt-8 w-full text-sm">
+                  Start shopping
+                </Button>
+              </SheetPrimitive.Close>
+            </div>
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
--- src/components/ProductItems.tsx
+++ src/components/ProductItems.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { buttonVariants } from "./ui/Button";
+import { Button } from "./ui/Button";
+import { useShoppingCart } from "@/lib/store";
 
 type ProductItemProps = {
   id: number;
@@ -19,13 +20,7 @@ export default function ProductItems({
   price,
 }: ProductItemProps) {
   const router = useRouter();
-  //   const { increaseQuantity } = useShoppingCart();
-
-  //   const navigate = useNavigate();
-
-  //   function navigateProduct(id: number) {
-  //     navigate(`/product/${id}`);
-  //   }
+  const { increaseCart } = useShoppingCart();
 
   return (
     <header className="border w-64 h-82 rounded shadow-md">
@@ -42,7 +37,7 @@ export default function ProductItems({
         <h2 className="mt-1 font-medium text-primary">{name}</h2>
         <div className="mt-2 flex justify-between items-center content-center">
           <p className="font-bold text-2xl">${price}</p>
-          <button className={buttonVariants({})}>Add to cart</button>
+          <Button onClick={() => increaseCart}>Add to cart</Button>
         </div>
       </footer>
     </header>
